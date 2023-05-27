1c1e20d91fef3c3e32ce7eb1761d4b16068a5ba5
Steven Chang <stevenchang5000@gmail.com>
2023-05-27 13:07:16 +1000

    fn create_vendor

--- lib.rs
+++ lib.rs
@@ -2,7 +2,22 @@
 
 #[ink::contract]
 mod escrow {
+    use ink::storage::Mapping;
     use openbrush::{contracts::ownable::*, traits::Storage};
+
+    // === ENUMS ===
+    #[derive(Debug, PartialEq, Eq, scale::Encode, scale::Decode)]
+    #[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
+    pub enum EscrowError {
+        VendorAlreadyExists,
+    }
+
+    // === EVENTS ===
+    #[ink(event)]
+    pub struct CreateVendor {
+        #[ink(topic)]
+        caller: AccountId,
+    }
 
     // === STRUCTS ===
     #[derive(Debug, Clone, scale::Encode, scale::Decode)]
@@ -11,18 +26,27 @@ mod escrow {
         admin: AccountId,
     }
 
+    #[derive(scale::Decode, scale::Encode)]
+    #[cfg_attr(
+        feature = "std",
+        derive(scale_info::TypeInfo, ink::storage::traits::StorageLayout)
+    )]
+    #[derive(Debug, Clone)]
+    pub struct Vendor {}
+
     #[ink(storage)]
     #[derive(Default, Storage)]
     pub struct Escrow {
         #[storage_field]
         ownable: ownable::Data,
+        vendors: Mapping<AccountId, Vendor>,
     }
-
     impl Escrow {
         #[ink(constructor)]
         pub fn new() -> Self {
             let mut instance = Self::default();
             instance._init_with_owner(Self::env().caller());
+            instance.vendors = Mapping::default();
             instance
         }
 
@@ -31,6 +55,23 @@ mod escrow {
             Config {
                 admin: self.ownable.owner(),
             }
+        }
+
+        #[ink(message)]
+        pub fn create_vendor(&mut self) -> Result<(), EscrowError> {
+            let caller: AccountId = Self::env().caller();
+            if self.vendors.get(&caller).is_some() {
+                return Err(EscrowError::VendorAlreadyExists);
+            }
+
+            // Create vendor for caller
+            let vendor: Vendor = Vendor {};
+            self.vendors.insert(caller, &vendor);
+
+            // Emit event
+            self.env().emit_event(CreateVendor { caller });
+
+            Ok(())
         }
     }
 
@@ -57,6 +98,24 @@ mod escrow {
             let config = escrow.config();
             // * it returns the config
             assert_eq!(config.admin, accounts.alice);
+        }
+
+        #[ink::test]
+        fn test_create_vendor() {
+            let accounts = test_utils::accounts();
+            test_utils::change_caller(accounts.alice);
+            let mut escrow = Escrow::new();
+            // when account is not a vendor
+            // * it creates a vendor profile for account
+            // * it emits a CreateVendor event (TO DO AFTER HACKATHON)
+            let mut result = escrow.create_vendor();
+            assert!(result.is_ok());
+            assert!(escrow.vendors.get(&accounts.alice).is_some());
+
+            // when account is already a vendor
+            // * it raises an error
+            result = escrow.create_vendor();
+            assert_eq!(result, Err(EscrowError::VendorAlreadyExists));
         }
     }
 }
